8348ed66f31344915ea526eb5836d7f2e60c5c57
Fancy <67991595+cookie-xq@users.noreply.github.com>
2021-03-09 22:43:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,12 +26,21 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`嘿嘿`)
+        response.write(`
+        <!DOCTYPE html>
+            <head>
+                <link rel="stylesheet" href="/x">
+            </head>
+            <body>
+                <h1>标题</h1>
+                <script src='/y'></script>
+            </body>
+        `)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`h1{color: red;}`)
         response.end()
     } else if (path === '/y') {
         response.statusCode = 200
@@ -41,7 +50,7 @@ var server = http.createServer(function (request, response) {
     } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
+        response.write(`你访问的页面不存在`)
         response.end()
     }
 
